c89f5a4ae510c43db2b89d8fa4134053673785ab
Nicolas GONZALEZ <nicolasgonzalez180@gmail.com>
2025-08-26 15:16:10 +0200

    Initial commit

---
+++ lib/k/form/rb.rb
@@ -0,0 +1,10 @@
+require "k/form/rb/version"
+
+module K
+  module Form
+    module Rb
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/k/form/rb/version.rb
@@ -0,0 +1,7 @@
+module K
+  module Form
+    module Rb
+      VERSION = "0.1.0"
+    end
+  end
+end
